1de05ee27465d1fb5b026d6e1f145fe700894a78
Gildas <gildas.houlmann@gmail.com>
2020-09-25 11:30:52 +0200

    added failed registration e2e test

--- e2e_tests/login_test.js
+++ e2e_tests/login_test.js
@@ -8,4 +8,12 @@ Scenario('test something', (I) => {
     I.fillField('.input-register-confirm', 'pass');
     I.click('.btn-register');
     I.see('Registration successful');
+
+    I.amOnPage(':8080/stackunderflow');
+    I.click('Login');
+    I.fillField('.input-register-username', 'testUsername');
+    I.fillField('.input-register-password', 'pass');
+    I.fillField('.input-register-confirm', 'wrongpass');
+    I.click('.btn-register');
+    I.see('Error : Passwords do not match');
 });
