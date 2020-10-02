23a80bf1a71f9fda4bc82a5b08eec1db9ac044d2
Gildas Houlmann <gildas.houlmann@gmail.com>
2020-10-02 12:23:46 +0200

    hopefully fix context root not found

--- e2e_tests/askquestion_test.js
+++ e2e_tests/askquestion_test.js
@@ -1,10 +1,18 @@
+
 Feature('askQuestion');
 
-Scenario('test asking a question', (I) => {
-    I.amOnPage(':8080/stackunderflow/askquestion');
+Scenario('test asking a question', ({I}) => {
+    I.amOnPage(':8080/stackunderflow')
+    I.click('Sign in')
+    I.fillField('Nom d\'utilisateur', 'testUserName1')
+    I.fillField( 'Nom', 'testName1')
+    I.fillField( 'Prénom', 'testFirstName1')
+    I.fillField( 'Email', 'testEmail1')
+    I.fillField( 'Mot de passe', 'testPassword1')
+    I.fillField( 'Confirmer le mot de passe', 'testPassword1')
+    I.click('input[type="submit"]')
     I.click('Poser une question');
-    I.fillField('.title', 'tfTitle');
-    I.fillField('.question', 'tfText');
+    I.fillField('input[type="text"]', 'tfTitle');
+    I.fillField('.text', 'tfText');
     I.click('.bSubmitQuestion');
 });
-
