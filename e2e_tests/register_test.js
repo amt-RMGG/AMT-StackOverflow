373b001d24925f241a75d3ba78c634beb1eb35ac
Gildas Houlmann <gildas.houlmann@gmail.com>
2020-10-02 02:49:18 +0200

    finished registration and login tests, updated gitignore to ignore .war file

--- e2e_tests/register_test.js
+++ e2e_tests/register_test.js
@@ -5,6 +5,7 @@ Scenario('register to the site', ({ I }) => {
     I.click('Sign in')
     I.seeCurrentUrlEquals('http://localhost:8080/stackunderflow/register')
 
+    //Register
     I.fillField('Nom d\'utilisateur', 'testUserName')
     I.fillField( 'Nom', 'testName')
     I.fillField( 'Prénom', 'testFirstName')
@@ -13,21 +14,41 @@ Scenario('register to the site', ({ I }) => {
     I.fillField( 'Confirmer le mot de passe', 'testPassword')
     I.click('input[type="submit"]')
     I.seeCurrentUrlEquals('http://localhost:8080/stackunderflow/questions')
+
+    //Go to profile page
     I.click('testFirstName testName')
     I.seeCurrentUrlEquals('http://localhost:8080/stackunderflow/user')
     I.see('User profile')
 
+    //go to question creation page
     I.click('Poser une question')
     I.seeCurrentUrlEquals('http://localhost:8080/stackunderflow/askQuestion')
     I.see('Title')
 
+    //Disconnect
     I.click('Déconnexion')
     I.seeCurrentUrlEquals('http://localhost:8080/stackunderflow/home')
 
+    //log in again with created account
     I.click('Login')
     I.fillField('Nom d\'utilisateur', 'testUserName')
     I.fillField( 'Mot de passe', 'testPassword')
-    I.wait(10)
+    I.click('input[type="submit"]')
+    I.seeCurrentUrlEquals('http://localhost:8080/stackunderflow/questions')
+    I.see('Liste des questions')
+
+    //Try incorrect credentials
+    I.click('Déconnexion')
+    I.click('Login')
+    I.fillField('Nom d\'utilisateur', 'wrongName')
+    I.fillField( 'Mot de passe', 'testPassword')
+    I.click('input[type="submit"]')
+    I.seeCurrentUrlEquals('http://localhost:8080/stackunderflow/login')
+    I.fillField('Nom d\'utilisateur', 'testUserName')
+    I.fillField( 'Mot de passe', 'wrongPassword')
+    I.click('input[type="submit"]')
+    I.seeCurrentUrlEquals('http://localhost:8080/stackunderflow/login')
+
 });
 
 /*
