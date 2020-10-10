14df3c8ad00707adf87301d69dac294c0b60a7a9
Gildas Houlmann <gildas.houlmann@gmail.com>
2020-10-10 16:57:47 +0200

    regrouped docker deployment files in the docker folder

--- build-image.sh
+++ build-image.sh
@@ -5,10 +5,11 @@ mvn clean package
 FILE=target/stackunderflow.war
 if [ -f "$FILE" ]; then
     #copie le .war au même niveau que le dockerfile
-    echo "$FILE found, copying..."
-    mv $FILE .
-    echo "$FILE copy finished"
+    echo "$FILE found, transfering..."
+    mv $FILE docker/images/openliberty
+    echo "$FILE transfer finished"
     #construit l'image
+    cd docker/images/openliberty
     docker build -t amt/stackunderflow .
 else 
     >&2 echo "Error : $FILE not found"
