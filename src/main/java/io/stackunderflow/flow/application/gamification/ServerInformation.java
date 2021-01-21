dbf4e96dc0819cb65e5015b2a694cea73322051d
Robin Reuteler <reutelerr@hotmail.com>
2021-01-21 19:18:42 +0100

    trying to solve container connections...

--- src/main/java/io/stackunderflow/flow/application/gamification/ServerInformation.java
+++ src/main/java/io/stackunderflow/flow/application/gamification/ServerInformation.java
@@ -2,7 +2,7 @@ package io.stackunderflow.flow.application.gamification;
 
 public class ServerInformation {
     final static public int PORT = 8090;
-    final static public String ADRESS = "localhost";
+    final static public String ADRESS = "host.docker.internal";
     final static public String apiURL = "http://" + ADRESS + ":" + PORT;
     final static public String apiKey = "c4287505-9e1b-4d85-9563-c8a32727b58a";
     final static public int upvoteEventType = 3;
